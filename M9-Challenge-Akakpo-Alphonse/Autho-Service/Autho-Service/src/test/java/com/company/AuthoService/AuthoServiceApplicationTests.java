0b4974cc1cf5ea36184f403e04cd7801b86c3c05
alpnelson <alpnelson5@gmail.com>
2023-08-19 00:17:06 -0400

    new

---
+++ M9-Challenge-Akakpo-Alphonse/Autho-Service/Autho-Service/src/test/java/com/company/AuthoService/AuthoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.AuthoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
